c5e8252d573f37cce6e9efa01afd66195314d14f
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2017-02-16 19:57:16 +0700

    Same fix as #70 for RNDFPBannerView.m

--- ios/RNDFPBannerView.m
+++ ios/RNDFPBannerView.m
@@ -120,11 +120,12 @@ didReceiveAppEvent:(NSString *)name
 {
     [super layoutSubviews ];
 
-    _bannerView.frame = CGRectMake(
-                                   self.bounds.origin.x,
-                                   self.bounds.origin.x,
-                                   _bannerView.frame.size.width,
-                                   _bannerView.frame.size.height);
+    self.frame = CGRectMake(
+       self.bounds.origin.x,
+       self.bounds.origin.x,
+       _bannerView.frame.size.width,
+       _bannerView.frame.size.height);
+
     [self addSubview:_bannerView];
 }
 
