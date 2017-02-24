d56faa594fa5b5bce2c48bc440988ffb88b76d12
Álvaro Medina Ballester <xlasttrainhomex@gmail.com>
2017-02-24 12:01:10 +0100

    Update RNAdMobRewarded.h
    Added support for RN 0.40

--- ios/RNAdMobRewarded.h
+++ ios/RNAdMobRewarded.h
@@ -1,5 +1,11 @@
-#import "RCTBridgeModule.h"
+#if __has_include(<React/RCTEventDispatcher.h>)
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTBridgeModule.h>
+#else
 #import "RCTEventDispatcher.h"
+#import "RCTBridgeModule.h"
+#endif
+
 @import GoogleMobileAds;
 
 @interface RNAdMobRewarded : NSObject <RCTBridgeModule, GADRewardBasedVideoAdDelegate>
