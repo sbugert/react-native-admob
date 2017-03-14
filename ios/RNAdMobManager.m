28176767d59f153e9e2a0bd160352138d1229c15
Francisco Guimarães <francisco.cpg@gmail.com>
2017-03-14 14:43:20 -0300

    Fix ios and react native 0.42

--- ios/RNAdMobManager.m
+++ ios/RNAdMobManager.m
@@ -15,20 +15,7 @@ RCT_EXPORT_MODULE();
 
 - (UIView *)view
 {
-  return [[BannerView alloc] initWithEventDispatcher:self.bridge.eventDispatcher];
-}
-
-- (NSArray *) customDirectEventTypes
-{
-  return @[
-           @"onSizeChange",
-           @"onAdViewDidReceiveAd",
-           @"onDidFailToReceiveAdWithError",
-           @"onAdViewWillPresentScreen",
-           @"onAdViewWillDismissScreen",
-           @"onAdViewDidDismissScreen",
-           @"onAdViewWillLeaveApplication"
-           ];
+  return [[BannerView alloc] init];
 }
 
 - (dispatch_queue_t)methodQueue
@@ -40,5 +27,13 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_VIEW_PROPERTY(bannerSize, NSString);
 RCT_EXPORT_VIEW_PROPERTY(adUnitID, NSString);
 RCT_EXPORT_VIEW_PROPERTY(testDeviceID, NSString);
+
+RCT_EXPORT_VIEW_PROPERTY(onSizeChange, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onAdViewDidReceiveAd, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onDidFailToReceiveAdWithError, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onAdViewWillPresentScreen, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onAdViewWillDismissScreen, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onAdViewDidDismissScreen, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onAdViewWillLeaveApplication, RCTBubblingEventBlock)
 
 @end
