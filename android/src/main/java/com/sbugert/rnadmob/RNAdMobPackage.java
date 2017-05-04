b46ed16fd91627f51549d72bb916702591db74f9
Alexandre Bini <alexandrebini@gmail.com>
2017-05-04 12:10:47 -0300

    fixed widht/height

--- android/src/main/java/com/sbugert/rnadmob/RNAdMobPackage.java
+++ android/src/main/java/com/sbugert/rnadmob/RNAdMobPackage.java
@@ -28,8 +28,6 @@ public class RNAdMobPackage implements ReactPackage {
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        System.out.println("anythgaaaaaaa");
-
         List<ViewManager> managers = new ArrayList<>();
         managers.add(new RNAdMobBannerViewManager());
         managers.add(new RNPublisherBannerViewManager());
