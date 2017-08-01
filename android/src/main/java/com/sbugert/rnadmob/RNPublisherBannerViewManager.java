990d5c72efa95824e8b5d15ff4da4df32f38db0b
Koen Punt <koen@koenpunt.nl>
2017-08-01 18:19:23 +0200

    fix null pointer exception when no adsize is set

--- android/src/main/java/com/sbugert/rnadmob/RNPublisherBannerViewManager.java
+++ android/src/main/java/com/sbugert/rnadmob/RNPublisherBannerViewManager.java
@@ -117,16 +117,21 @@ class ReactPublisherAdView extends ReactViewGroup implements AppEventListener {
 
   public void loadBanner() {
     ArrayList<AdSize> adSizes = new ArrayList<AdSize>();
-    adSizes.add(this.adSize);
+    if (this.adSize != null) {
+      adSizes.add(this.adSize);
+    }
     if (this.validAdSizes != null) {
       for (int i = 0; i < this.validAdSizes.length; i++) {
         adSizes.add(this.validAdSizes[i]);
       }
     }
+
+    if (adSizes.size() == 0) {
+      adSizes.add(AdSize.BANNER);
+    }
+
     AdSize[] adSizesArray = adSizes.toArray(new AdSize[adSizes.size()]);
     this.adView.setAdSizes(adSizesArray);
-
-    // this.sendOnSizeChangeEvent();
 
     PublisherAdRequest.Builder adRequestBuilder = new PublisherAdRequest.Builder();
     if (testDevices != null) {
