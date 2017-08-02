fd391d23e09bc732abfc513637ba6d660d2079ce
Koen Punt <koen@koenpunt.nl>
2017-08-03 00:17:48 +0200

    Proposal: Targeting support
    I'm not sure if using nested prop is the right approach, so if you have any suggestions, please let me know!

--- RNPublisherBanner.js
+++ RNPublisherBanner.js
@@ -6,7 +6,7 @@ import {
   findNodeHandle,
   ViewPropTypes,
 } from 'react-native';
-import { string, func, arrayOf } from 'prop-types';
+import { string, func, arrayOf, bool, object, shape, instanceOf, oneOf, number } from 'prop-types';
 
 import { createErrorFromErrorData } from './utils';
 
@@ -119,6 +119,62 @@ PublisherBanner.propTypes = {
   onAdViewDidDismissScreen: func,
   onAdViewWillLeaveApplication: func,
   onAdmobDispatchAppEvent: func,
+
+  targeting: shape({
+    /**
+     * Arbitrary object of custom targeting information.
+     */
+    customTargeting: object,
+
+    /**
+     * Array of exclusion labels.
+     */
+    categoryExclusions: arrayOf(string),
+
+    /**
+     * Array of keyword strings.
+     */
+    keywords: arrayOf(string),
+
+    /**
+     * When using backfill or an SDK mediation creative, gender can be supplied
+     * in the ad request for targeting purposes.
+     */
+    gender: oneOf(['unknown', 'male', 'female']),
+
+    /**
+     * When using backfill or an SDK mediation creative, birthday can be supplied
+     * in the ad request for targeting purposes.
+     */
+    birthday: instanceOf(Date),
+
+    /**
+     * Indicate that you want Google to treat your content as child-directed.
+     */
+    childDirectedTreatment: bool,
+
+    /**
+     * Applications that monetize content matching a webpage's content may pass
+     * a content URL for keyword targeting.
+     */
+    contentURL: string,
+
+    /**
+     * You can set a publisher provided identifier (PPID) for use in frequency
+     * capping, audience segmentation and targeting, sequential ad rotation, and
+     * other audience-based ad delivery controls across devices.
+     */
+    publisherProvidedID: string,
+
+    /**
+     * The user’s current location may be used to deliver more relevant ads.
+     */
+    location: shape({
+      latitude: number,
+      longitude: number,
+      accuracy: number,
+    }),
+  }),
 };
 
 const RNDFPBannerView = requireNativeComponent('RNDFPBannerView', PublisherBanner);
