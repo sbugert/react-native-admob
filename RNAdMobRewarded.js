fcb1f5aa60be3a017b18776b9bda6118f1ca7265
Koen Punt <koen@koenpunt.nl>
2017-10-03 23:49:32 +0200

    add namespace to events (#192)
    since events are not scoped to a module they did clash.

--- RNAdMobRewarded.js
+++ RNAdMobRewarded.js
@@ -7,35 +7,55 @@ import { createErrorFromErrorData } from './utils';
 
 const RNAdMobRewarded = NativeModules.RNAdMobRewarded;
 
-const adMobRewardedEmitter = new NativeEventEmitter(RNAdMobRewarded);
+const eventEmitter = new NativeEventEmitter(RNAdMobRewarded);
 
-const eventHandlers = {};
+const eventMap = {
+  adLoaded: 'rewardedVideoAdLoaded',
+  adFailedToLoad: 'rewardedVideoAdFailedToLoad',
+  adOpened: 'rewardedVideoAdOpened',
+  adClosed: 'rewardedVideoAdClosed',
+  adLeftApplication: 'rewardedVideoAdLeftApplication',
+  rewarded: 'rewardedVideoAdRewarded',
+  videoStarted: 'rewardedVideoAdVideoStarted',
+};
 
-const addEventListener = (type, handler) => {
-  eventHandlers[type] = eventHandlers[type] || new Map();
-  if (type === 'adFailedToLoad') {
-    eventHandlers[type].set(handler, adMobRewardedEmitter.addListener(type, error => handler(createErrorFromErrorData(error))));
+const _subscriptions = new Map();
+
+const addEventListener = (event, handler) => {
+  const mappedEvent = eventMap[event];
+  if (mappedEvent) {
+    let listener;
+    if (event === 'adFailedToLoad') {
+      listener = eventEmitter.addListener(mappedEvent, error => handler(createErrorFromErrorData(error)));
+    } else {
+      listener = eventEmitter.addListener(mappedEvent, handler);
+    }
+    _subscriptions.set(handler, listener);
+    return {
+      remove: () => removeEventListener(event, handler)
+    };
   } else {
-    eventHandlers[type].set(handler, adMobRewardedEmitter.addListener(type, handler));
+    console.warn(`Trying to subscribe to unknown event: "${event}"`);
+    return {
+      remove: () => {},
+    };
   }
 };
 
 const removeEventListener = (type, handler) => {
-  if (!eventHandlers[type].has(handler)) {
+  const listener = _subscriptions.get(handler);
+  if (!listener) {
     return;
   }
-  eventHandlers[type].get(handler).remove();
-  eventHandlers[type].delete(handler);
+  listener.remove();
+  _subscriptions.delete(handler);
 };
 
 const removeAllListeners = () => {
-  const types = Object.keys(eventHandlers);
-  types.forEach(type => (
-    eventHandlers[type].forEach((subscription, key, map) => {
-      subscription.remove();
-      map.delete(key);
-    })
-  ));
+  _subscriptions.forEach((listener, key, map) => {
+    listener.remove();
+    map.delete(key);
+  });
 };
 
 export default {
