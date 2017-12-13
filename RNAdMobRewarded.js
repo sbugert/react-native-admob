74370d15355755141f3d0e0363fa77495b7dd4e5
Koen Punt <koen@koenpunt.nl>
2017-12-13 10:56:57 +0100

    perf: lazy load each module, update eslint config

--- RNAdMobRewarded.js
+++ RNAdMobRewarded.js
@@ -32,9 +32,10 @@ const addEventListener = (event, handler) => {
     }
     _subscriptions.set(handler, listener);
     return {
-      remove: () => removeEventListener(event, handler)
+      remove: () => removeEventListener(event, handler),
     };
   } else {
+    // eslint-disable-next-line no-console
     console.warn(`Trying to subscribe to unknown event: "${event}"`);
     return {
       remove: () => {},
