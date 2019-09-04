043cfafb3c73317eb838517718a3b12ebe47f8df
Richard <richlloydmiles@gmail.com>
2019-09-04 11:03:30 +0200

    Update RNPublisherBanner.js

--- RNPublisherBanner.js
+++ RNPublisherBanner.js
@@ -10,7 +10,7 @@ import { string, func, arrayOf } from 'prop-types';
 import { createErrorFromErrorData } from './utils';
 
 class PublisherBanner extends Component {
-  
+
   constructor() {
     super();
     this.handleSizeChange = this.handleSizeChange.bind(this);
