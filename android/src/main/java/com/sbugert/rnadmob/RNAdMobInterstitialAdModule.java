0eff1b0d0b17676364dc5add67b3a186bc57545f
ermankuruoglu <ermankuruoglu@hotmail.com>
2019-09-18 01:39:29 +0300

    fix for 'cannot find symbol @Nullable' build error

--- android/src/main/java/com/sbugert/rnadmob/RNAdMobInterstitialAdModule.java
+++ android/src/main/java/com/sbugert/rnadmob/RNAdMobInterstitialAdModule.java
@@ -2,7 +2,7 @@ package com.sbugert.rnadmob;
 
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
